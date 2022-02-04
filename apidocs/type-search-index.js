174bb7c21de2a2840f761bca917abd53755e827a
Laird Nelson <ljnelson@gmail.com>
2022-02-04 05:13:05 +0000

    Building Github Pages site for microbean-constant 0.0.2

--- apidocs/type-search-index.js
+++ apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.microbean.constant","l":"Constables"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.microbean.constant","l":"Constables"},{"p":"org.microbean.constant","l":"ConstantDescs"}];updateSearchResults();
